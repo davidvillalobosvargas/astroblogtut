186eec5fd8c039afd2afebb574cffba8160e6948
David Villalobos Vargas <davidvillalobosvargas@gmail.com>
2024-01-21 21:08:09 -0600

    set site to astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://stargazers.club',
+  site: 'https://silly-licorice-d7cfee.netlify.app/',
   integrations: [icon(), tailwind(), sitemap(), react()]
 });
